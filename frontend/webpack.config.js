2b7048377ab14fdb7e50fec8b69c0b4daeccaf3f
yyh1102 <234347589@qq.cim>
2017-03-10 21:24:24 +0800

    debug false

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,7 +2,7 @@ var webpack=require("webpack");
 var HtmlWebpackPlugin=require("html-webpack-plugin");
 var ExtractTextPlugin=require("extract-text-webpack-plugin");
 var path=require("path");
-var debug=true;
+var debug=false;
 
 module.exports={
     entry: {
